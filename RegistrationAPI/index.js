308c464f3913476a0340d84de2431c5e493ce6e7
Rajma Chaawal! <luckysharma0729@gmail.com>
2026-08-23 23:05:27 +0530

    I learned user creation today on using mongoose! and how modelling and schemas work!

--- RegistrationAPI/index.js
+++ RegistrationAPI/index.js
@@ -1,15 +1,30 @@
 require("dotenv").config();
+const User = require('./Models/user')
 
 const mongoose = require("mongoose");
 
-function connectDB() {
-    mongoose.connect(process.env.MONGODB_URI)
-    .then(() => {
-        console.log("MongoDB connected successfully!");
-    })
-    .catch((error) => {
-        console.error("MongoDB connection failed:", error);
-    });
-}
+async function start() {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
 
-connectDB();
+        console.log("MongoDB connected!");
+
+        const user = new User({
+            name: "Lucky",
+            email: "lucky@example.com",
+            age: 20
+        });
+
+        try {
+            const savedUser = await user.save();
+            console.log(`User ${savedUser.name} created successfully!`);
+            console.log(`User ID: ${savedUser._id}!`);
+        }catch(error) {
+            console.error("User Creation Error:", error);
+        }
+
+    } catch (error) {
+        console.error("MongoDB Connection Error:", error);
+    }
+}
+start();
