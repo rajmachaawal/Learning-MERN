3fe0c256c3a2199cee076e759feef11c7067d789
Rajma Chaawal! <luckysharma0729@gmail.com>
2026-08-24 18:55:46 +0530

    I have learned about how to create a record of any entity in my collection! and how to find all, find one and find by using _id! I have also grasped the concept of using comparison operators for filtering records on the basis of numeric parameters!

--- README.md
+++ README.md
@@ -9,3 +9,8 @@ Created a simple weather app that fetches weather data of a location from weathe
 # 21-08-2026
 
 I started learning API creation, starting with registration API, used mongoose to establish connection with MongoDB cluster! and for the next session plan on to modelling a user record with mongoose and a create a user record on registration with username and password! also to store username and password in hashed and salted form! TILL THEN PEACE OUT!
+
+
+# 24-08-2026
+
+Since last session, I have learned about how to create a record of any entity in my collection! and how to find all, find one and find by using _id! I have also grasped the concept of using comparison operators for filtering records on the basis of numeric parameters! I have realised on thing that storing a user's credentials in database is quite far! But we will get there much sooner at our pace! TILL NEXT TIME! PEACE OUT!
--- RegistrationAPI/index.js
+++ RegistrationAPI/index.js
@@ -1,30 +1,108 @@
 require("dotenv").config();
 const User = require('./Models/user')
-
 const mongoose = require("mongoose");
 
+
+
+//USER CREATING FUNCTION
+async function createUser(userName, userEmail, userAge) {
+    try {
+        const user = newUser({
+            name: userName,
+            email: userEmail,
+            age: userAge
+        })
+        const savedUser = await user.save();
+        console.log(`User ${savedUser.name} created successfully!`);
+        console.log(`User ID: ${savedUser._id}!`);
+    }catch(error){
+        console.error("Error creating user: ", error.message);
+    }
+
+}
+
+
+
+//ALL USERS FETCHING FUNCTION
+async function getUsers() {
+    try{
+        const users = await User.find();
+        console.log("Users:", users);
+    }catch(error) {
+        console.error("Error fetching users: ", error);
+    }
+}
+
+
+
+//FUNCTIONS FOR FETCHING USER BY EMAIL
+async function getUserByEmail(userEmail) {
+    try{
+        const user = await User.findOne({
+            email: userEmail
+        })
+        if(user !== null) {
+            console.log(`User with email -${userEmail} "Found": \n${user.name} \n${user.email} \n${user.age}`);
+        }else{
+            console.log(`User with email -${userEmail} "Not found".`);
+        }
+    }catch(error){
+        console.error("Error fetching user by email: ", error);
+    }
+}
+
+
+
+//FUNCTION FOR FECHING BY ID
+async function getUserById(userId) {
+    try{
+        const user = await User.findById(userId);
+        if(user !== null) {
+            console.log(`User with ID -${userId} "Found": \n${user.name} \n${user.email} \n${user.age}`);
+        }else{
+            console.log(`User with ID -${userId} "Not found".`);
+        }
+    }catch(error){
+        console.error("Error fetching user by ID: ", error.message);
+    }
+}
+
+
+
+//FUNCTION FOR FETCHING WITH AGE FILTER
+async function getUserByAge(someAge) {
+    try{
+        let userNumber = 1;
+        const users = await User.find({
+            age: {
+                $gt: someAge
+            }
+        });
+        if(users.length !== 0) {
+            console.log(`Users with age greater than ${someAge}:\n`);
+            for(const user of users) {
+                console.log(`User ${userNumber}: \n ${user.name} \n ${user.email} \n ${user.age}\n`);
+                userNumber++;
+            }
+        } else {
+            console.log(`No users found with age greater than ${someAge}`);
+        }
+    }catch(error) {
+        console.error("Error fetching users by age: ", error.message);
+    }
+}
+
+//MONGODB CONNECTION
 async function start() {
     try {
         await mongoose.connect(process.env.MONGODB_URI);
 
         console.log("MongoDB connected!");
 
-        const user = new User({
-            name: "Lucky",
-            email: "lucky@example.com",
-            age: 20
-        });
-
-        try {
-            const savedUser = await user.save();
-            console.log(`User ${savedUser.name} created successfully!`);
-            console.log(`User ID: ${savedUser._id}!`);
-        }catch(error) {
-            console.error("User Creation Error:", error);
-        }
+        getUserByAge(20);
 
     } catch (error) {
-        console.error("MongoDB Connection Error:", error);
+        return console.error("MongoDB Connection Error:", error);
     }
 }
 start();
